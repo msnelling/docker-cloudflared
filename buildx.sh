f28bad24e8a3982aeb8196d6ea071a84a4cde5fb
Mark Snelling <mark@bakedbeans.com>
2020-11-12 21:21:09 +0000

    Updated buildx script.

--- buildx.sh
+++ buildx.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t msnelling/cloudflared:latest --push .
+docker buildx build --pull --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7 -t msnelling/cloudflared:latest --push .
